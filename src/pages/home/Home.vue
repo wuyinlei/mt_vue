de378a9ae249ca23d3db5f3e4e6b506e5f286a47
wuyinlei <power1118wu@gmail.com>
2018-05-03 18:41:58 +0800

    1.axios的引入和使用 2.api的使用和线上线下重定向(开发为测试环境)

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -27,6 +27,7 @@
   import HomeIcons from './components/Icons'
   import HomeRecommend from './components/Recommend'
   import HomeWeekend from './components/Weekend'
+  import axios from 'axios'
 
   export default {
     name: 'Home',
@@ -35,8 +36,19 @@
       HomeSwiper: HomeSwiper,
       HomeIcons: HomeIcons,
       HomeRecommend: HomeRecommend,
-      HomeWeekend:HomeWeekend
-    }
+      HomeWeekend: HomeWeekend
+    }, methods: {
+      getHomeInfo() {
+        axios.get('/api/index.json')
+          .then(this.getHomeInfoSucc)
+      },
+      getHomeInfoSucc(res) {
+        console.log(res)
+      }
+    },
+    mounted() {
+      this.getHomeInfo()
+    },
   }
 </script>
 
